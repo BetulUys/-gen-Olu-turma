2e467e81e0abe38c44682fcc58559d93cda3a535
BetulUys <100969641+BetulUys@users.noreply.github.com>
2022-04-17 20:57:47 +0300

    Add files via upload

---
+++ UcHarfliUniq.java
@@ -0,0 +1,34 @@
+package advenced02;
+
+import java.util.Scanner;
+
+public class UcHarfliUniq {
+    public static void main(String[] args) {
+        /* TASK :
+         * Kullanicidan bir isim isteyiniz ve isim sadece 3 harfli olmalidir ternary
+         * kullanarak harflerin unique(farkli) olup olmadigina bakiniz e.g: ala => 3
+         * Test data: ali eme all
+         */
+        Scanner scan = new Scanner(System.in);
+        System.out.print("agam bir isim gir : ");
+        String  isim =scan.nextLine();
+
+        System.out.println(isim.length()==3?
+                ((isim.substring(0,1)!=isim.substring(1,2)&&isim.substring(0,1)!=isim.substring(2,3)&&isim.substring(1,2)!=isim.substring(2,3))?
+                        "3 harfli Uniq ": "3 harfli uniq değil"):"uniq değil");//düzgün çalışmıyor sor
+
+
+
+
+
+
+
+
+        /*char c1=isim.charAt(0);
+        char c2=isim.charAt(1);
+        char c3=isim.charAt(2);
+        String  sonuc =  isim.length()==3 ?((c1!=c2 && c1!=c3 && c2!=c3)?"girdiginiz isim 3 harfli ve uniq karaktere sahip":
+                "girdiginiz isim 3 harfli ve uniq karaktere sahip değil"):"Girdiginiz isim 3 harfli değil";
+        System.out.println(sonuc);
+*/    }
+}
